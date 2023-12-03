803d8095aa7287e468d896e070122d3b8d8a2844
willamessantos <willamessantos@live.com>
2023-12-02 23:32:59 -0300

    feat: create history user use case

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,6 +1,7 @@
 import { Prisma, CheckIn } from '@prisma/client'
 
 export interface CheckInsRepository {
-  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  findManyByUserId(userId: string): Promise<CheckIn[]>
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
 }
