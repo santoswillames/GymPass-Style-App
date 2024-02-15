5b3c43afc1e90d4020c0b05087f36ea89e09607b
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2024-02-15 08:59:16 -0300

    feat(auth): create jwt function

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -16,7 +16,17 @@ export async function authenticate(
 
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({ token })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
@@ -24,6 +34,4 @@ export async function authenticate(
 
     throw error
   }
-
-  return reply.status(200).send()
 }
