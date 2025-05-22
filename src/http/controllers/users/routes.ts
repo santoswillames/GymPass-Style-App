f9b15ec368626ff9d3074b93778d4b156a57961f
Willames Santos <willamessantos@live.com>
2025-05-22 10:18:22 -0300

    create refresh token route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated - Rotas que só poderam ser chamadas quando o usuário estiver autenticado. */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
