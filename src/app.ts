2f17f2766893d23a62d6c0c8a231bc3b051a31b8
Willames Santos <willamessantos@live.com>
2025-05-22 10:33:59 -0300

    create refresh token route test e2e

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ export const app = fastify()
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
   cookie: {
-    cookieName: 'reafreshToken',
+    cookieName: 'refreshToken',
     signed: false,
   },
   sign: {
